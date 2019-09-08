197fbc62e4746a22fd81793637290dba73e2976d
Mohammad Nurdin (Zero) <mohammadnrdn@gmail.com>
2019-09-09 06:17:04 +0800

    add provider

---
+++ src/provider/provider.ts
@@ -0,0 +1,20 @@
+import { Injectable } from'@angular/core';
+import {Http, Headers, RequestOptions } from'@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class Provider {
+
+    server: string = "http://localhost:8888/todolist-api/";
+
+  constructor(public http : Http) {    
+  }
+
+  postData(body, file) {
+    let type = "application/json; charset=UTF-8";
+    let headers = new Headers({ "Content-Type": type});
+    let options = new RequestOptions({headers: headers});
+
+    return this.http.post(this.server + file, JSON.stringify(body), options).map(res => res.json())
+  }
+}
