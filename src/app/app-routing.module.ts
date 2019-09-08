ea69368d00754c63708e04bc9514fd361d7ab2d4
Mohammad Nurdin (Zero) <mohammadnrdn@gmail.com>
2019-09-09 04:22:03 +0800

    update root page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'index', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'index', loadChildren: './index/index.module#IndexPageModule' },
+  { path: 'create', loadChildren: './create/create.module#CreatePageModule' },
+  { path: 'show', loadChildren: './show/show.module#ShowPageModule' },
 ];
 
 @NgModule({
